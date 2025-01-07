2611d4e2105bb7c8d3a3716fb6b516a59bcfc517
Botan Elveren <botanelveren@gmail.com>
2025-01-07 14:23:44 +0300

    Add project files.

---
+++ FirstClassApp/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClassApp
+{
+    public class Person
+    {
+        public string name;
+        public string lastName;
+        public string birthOfDate;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+
+        }
+
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string BirthOfDate
+        {
+            get
+            {
+                return birthOfDate;
+            }
+            set
+            {
+                birthOfDate = value;
+            }
+        }
+    }
+}
---
+++ FirstClassApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using FirstClassApp;
+
+Person student1 = new Person();
+student1.Name = "Botan";
+student1.LastName = "Elveren";
+student1.BirthOfDate = "30.06.1990";
+
+Console.WriteLine($"Student1: {student1.Name} {student1.LastName} {student1.BirthOfDate}");
+
+Person student2 = new Person();
+student2.Name = "Mehmet";
+student2.LastName = "Elveren";
+student2.BirthOfDate = "1.02.1970"; 
+
+Console.WriteLine($"Student2: {student2.Name} {student2.LastName} {student2.BirthOfDate}");
+
+Person teacher1 = new Person();
+teacher1.Name = "Ali";
+teacher1.LastName = "Kaya";
+teacher1.BirthOfDate = "1.02.1980";
+
+Console.WriteLine($"Teacher1: {teacher1.Name} {teacher1.LastName} {teacher1.BirthOfDate}");
+
+Person teacher2 = new Person();
+teacher2.Name = "Veli";
+teacher2.LastName = "Kahraman";
+teacher2.BirthOfDate = "1.02.1973";
+
+Console.WriteLine($"Teacher1: {teacher1.Name} {teacher1.LastName} {teacher1.BirthOfDate}");
